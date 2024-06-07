bdf735443627b90d11f2bcf1499b15c51de9fa42
1ureka <summerheadquarters001@gmail.com>
2024-06-07 23:48:36 +0800

    lint

--- src/components/(home)/display/HomeTitle.tsx
+++ src/components/(home)/display/HomeTitle.tsx
@@ -5,7 +5,7 @@ export default function HomeTitle() {
     <Stack direction="row" alignItems="center" spacing={3.5}>
       <Avatar sx={{ width: 56, height: 56 }} />
       <Typography variant="h4" sx={{ letterSpacing: "0.2rem" }}>
-        1UREKA's SPACE
+        {"1UREKA's SPACE"}
       </Typography>
     </Stack>
   );

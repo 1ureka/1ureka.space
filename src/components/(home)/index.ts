ef46b5db0e4ec50236eb697327b5c7a16cb35389
1ureka <summerheadquarters001@gmail.com>
2024-06-11 23:09:44 +0800

    del

--- src/components/(home)/index.ts
+++ src/components/(home)/index.ts
@@ -1,5 +1,1 @@
-export { default as ThemeMenu } from "./input/ThemeMenu";
-export { default as AnimateMenu } from "./input/AnimateMenu";
-export { default as UserInput } from "./input/UserInput";
-export { default as PasswordInput } from "./input/PasswordInput";
-export { default as HomeTitle } from "./display/HomeTitle";
+export { default as NavCard } from "./input/NavCard";
---
+++ src/components/(home)/input/NavCard.tsx
@@ -0,0 +1,3 @@
+export default function NavCard() {
+  return <div>NavCard</div>;
+}

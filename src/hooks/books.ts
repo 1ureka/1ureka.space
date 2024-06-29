7d7d704ef06dc9802b6754fe9abd31d30f77d5ae
1ureka <summerheadquarters001@gmail.com>
2024-06-29 11:52:23 +0800

    u

--- src/hooks/books.ts
+++ src/hooks/books.ts
@@ -4,18 +4,16 @@ import { useEffect } from "react";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { BOOKS_IS_EXPANDED, BOOKS_SELECT_GROUP } from "@/context/store";
 import { BOOKS_CAROUSELS } from "@/context/store";
-
-type ImageListItem = { name: string; group: string };
-type GroupLists = { [group: string]: string[] };
+import type { Metadata } from "@/data/table";
 
 /**
- * 用於圖庫畫廊的功能。
+ * 提供畫廊組件所需的資料與功能。
  * @returns 一個物件，包含以下屬性：
- * - `imagesByGroup`: 以組別為鍵，圖片名稱陣列為值的物件。
- * - `imagesCount`: 應該要顯示的圖片數量。
- * - `isGroupExpanded`: 判斷特定組別是否展開的函式。
+ *   - `metadataListByGroup`: 一個物件，其中鍵為組名，值為屬於該組的 metadata 物件陣列。
+ *   - `count`: 畫廊中應顯示的項目數量。如果所有組都展開，則為書籍總數；如果僅展開選定的組，則為組數。
+ *   - `isGroupExpanded`: 一個函式，接受組名並返回 `true`（如果組已展開）或 `false`（如果組未展開）。
  */
-export const useBooksGallery = (imageList: ImageListItem[]) => {
+export const useBooksGallery = (metadataList: Metadata[]) => {
   const isExpanded = useRecoilValue(BOOKS_IS_EXPANDED);
   const [selectGroup, setSelectGroup] = useRecoilState(BOOKS_SELECT_GROUP);
 
@@ -25,38 +23,35 @@ export const useBooksGallery = (imageList: ImageListItem[]) => {
     return () => setSelectGroup("");
   }, [setSelectGroup]);
 
-  const imagesByGroup: GroupLists = imageList.reduce(
-    (acc: GroupLists, curr: ImageListItem) => {
+  type MetadataListByGroup = { [group: string]: Metadata[] };
+  const metadataListByGroup: MetadataListByGroup = metadataList.reduce(
+    (acc: MetadataListByGroup, curr: Metadata) => {
       acc[curr.group] = acc[curr.group] || [];
-      acc[curr.group].push(curr.name);
+      acc[curr.group].push(curr);
 
       return acc;
     },
     {}
   );
 
-  Object.keys(imagesByGroup).forEach((g) => {
-    imagesByGroup[g].sort();
-  });
+  const count = isExpanded
+    ? metadataList.length
+    : Object.keys(metadataListByGroup).length;
 
-  const imagesCount = isExpanded
-    ? imageList.length
-    : Object.keys(imagesByGroup).length;
-
-  return { imagesByGroup, imagesCount, isGroupExpanded };
+  return { metadataListByGroup, count, isGroupExpanded };
 };
 
 /**
  * 用於處理圖片或組別點擊事件。
  * @returns 點擊處理函式。
  */
-export const useBooksButtonHandler = ({ name, group }: ImageListItem) => {
+export const useBooksButtonHandler = ({ group, index }: Metadata) => {
   const isExpanded = useRecoilValue(BOOKS_IS_EXPANDED);
   const setCarousels = useSetRecoilState(BOOKS_CAROUSELS);
   const [selectGroup, setSelectGroup] = useRecoilState(BOOKS_SELECT_GROUP);
 
   const handleImageClick = () => {
-    setCarousels(name);
+    setCarousels(index);
   };
 
   const handleGroupClick = () => {
@@ -66,4 +61,12 @@ export const useBooksButtonHandler = ({ name, group }: ImageListItem) => {
   return isExpanded || selectGroup === group
     ? handleImageClick
     : handleGroupClick;
+};
+
+export const useCarousels = () => {
+  const setCarousels = useSetRecoilState(BOOKS_CAROUSELS);
+
+  useEffect(() => {
+    setCarousels(-1);
+  }, [setCarousels]);
 };

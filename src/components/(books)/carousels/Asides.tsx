187f70da0f8b93f73b294f18d453e2833b07d1f7
1ureka <summerheadquarters001@gmail.com>
2024-06-29 20:06:45 +0800

    u asides

--- src/components/(books)/carousels/Asides.tsx
+++ src/components/(books)/carousels/Asides.tsx
@@ -1,5 +1,12 @@
+"use client";
+
+import { Major_Mono_Display } from "next/font/google";
+import { useEffect } from "react";
+import { useMotionTemplate, useSpring } from "framer-motion";
+
 import { Box, Stack, Typography } from "@mui/material";
 import PhotoRoundedIcon from "@mui/icons-material/PhotoRounded";
+import { StackM } from "@/components/Motion";
 import { RightClickIcon, LeftClickIcon } from "..";
 
 export default function Asides({
@@ -29,8 +36,71 @@ export default function Asides({
   );
 }
 
+const majorMonoDisplay = Major_Mono_Display({
+  weight: "400",
+  subsets: ["latin"],
+  fallback: ["monospace"],
+});
+
+const typoSx = {
+  color: "text.secondary",
+  lineHeight: "normal",
+  ...majorMonoDisplay.style,
+} as const;
+
+function Numbers({ type, num }: { type: "large" | "small"; num: string }) {
+  const spring = useSpring(0, { stiffness: 37, damping: 8, mass: 0.3 });
+  const y = useMotionTemplate`${spring}%`;
+
+  useEffect(() => {
+    spring.set(-parseInt(num) * 100);
+  }, [spring, num]);
+
+  const numbers = Array(10)
+    .fill(0)
+    .map((_, i) => i);
+  console.log(numbers);
+
+  return (
+    <Box sx={{ position: "relative", overflow: "hidden" }}>
+      <StackM style={{ y }}>
+        {numbers.map((i) => (
+          <Typography
+            key={i}
+            component="span"
+            variant={type === "large" ? "h2" : "h4"}
+            sx={{
+              position: i !== 0 ? "absolute" : "",
+              top: i !== 0 ? `${i * 100}%` : "",
+              ...typoSx,
+            }}
+          >
+            {i}
+          </Typography>
+        ))}
+      </StackM>
+    </Box>
+  );
+}
+
 function Indicator({ current, total }: { current: number; total: number }) {
-  return <div>{current + 1 + " / " + total}</div>;
+  const currentStr = current.toString().padStart(3, "0").split("");
+  const totalStr = total.toString().padStart(2, "0").split("");
+
+  return (
+    <Stack direction="row" alignItems="flex-end">
+      <Numbers type="large" num={currentStr[0]} />
+      <Numbers type="large" num={currentStr[1]} />
+      <Numbers type="large" num={currentStr[2]} />
+
+      <Typography component="span" variant="h2" sx={typoSx}>
+        {"/"}
+      </Typography>
+
+      <Numbers type="small" num={totalStr[0]} />
+      <Numbers type="small" num={totalStr[1]} />
+    </Stack>
+  );
 }
 
 function Hint() {

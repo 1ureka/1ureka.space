ed9af21b8aa7b31984e5f95c6b4ad5d56a6bc3d7
1ureka <summerheadquarters001@gmail.com>
2024-07-03 20:59:13 +0800

    u

--- src/components/(files)/index.ts
+++ src/components/(files)/index.ts
@@ -5,17 +5,10 @@ export { default as VerButton } from "./input/VerButton";
 
 export { default as Alert } from "./display/Alert";
 
-export * from "./tableF/TableHeaderF";
-export * from "./tableF/TableHeadF";
-export * from "./tableF/TableBodyF";
-export * from "./tableF/TableFooterF";
-
-export * from "./table/TableHeader";
-export * from "./table/TableHead";
-export * from "./table/TableBody";
-export * from "./table/TableFooter";
+export * from "./display/TableHeader";
+export * from "./display/TableHead";
+export * from "./display/TableBody";
+export * from "./display/TableFooter";
 
 export { default as Options } from "./block/Options";
-export { default as OptionsF } from "./block/OptionsF";
 export { default as Table } from "./block/Table";
-export { default as TableF } from "./block/TableF";

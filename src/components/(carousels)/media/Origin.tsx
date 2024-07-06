a7f8432019117e93f53295f79edc1d5244bca714
1ureka <summerheadquarters001@gmail.com>
2024-07-06 23:15:56 +0800

    u origin

--- src/components/(carousels)/media/Origin.tsx
+++ src/components/(carousels)/media/Origin.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import type { ImageMetadataWithIndex } from "@/data/type";
-import { delay } from "@/utils/client-utils";
-import { useCarouselIndex } from "@/hooks";
+import { useCarouselIndex, useDecode } from "@/hooks";
 
-import Image from "next/image";
-import { BoxM } from "@/components/Motion";
 import { carouselsOriginVar } from "@/components/MotionProps";
+import { motion } from "framer-motion";
 
 export default function Origin({
   metadataList,
@@ -16,42 +13,21 @@ export default function Origin({
 }) {
   const index = useCarouselIndex(metadataList);
   const metadata = metadataList[index];
-
-  const [originSrc, setOriginSrc] = useState<string | undefined>();
-  const [loadingO, setLoadingO] = useState(true);
-
-  useEffect(() => {
-    let isCurr = true;
-    setLoadingO(true);
-
-    (async () => {
-      await delay(250);
-      if (!isCurr) return;
-      console.log("fetch origin", metadata);
-      setOriginSrc(`/api/image/${metadata.id}/origin`);
-    })();
-
-    return () => {
-      isCurr = false;
-    };
-  }, [metadata]);
+  const [src, state] = useDecode(`/api/image/${metadata.id}/origin`);
 
   return (
-    originSrc && (
-      <BoxM
-        variants={carouselsOriginVar}
-        animate={loadingO ? "hide" : "show"}
-        sx={{ position: "absolute", width: 1, height: 1 }}
-      >
-        <Image
-          unoptimized
-          fill
-          src={originSrc}
-          alt={metadata.name}
-          onLoad={() => setLoadingO(false)}
-        />
-      </BoxM>
-    )
+    <motion.img
+      variants={carouselsOriginVar}
+      animate={state ? "show" : "hide"}
+      src={src}
+      alt={metadata.name}
+      style={{
+        display: "block",
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+      }}
+    />
     // {/* 注意全螢幕組件與loadingO的關係!! */}
   );
 }

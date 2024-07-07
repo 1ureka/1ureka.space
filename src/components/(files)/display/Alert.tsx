e765cc5c3ab9123006a1e3e2c3fd15686e337dc9
1ureka <summerheadquarters001@gmail.com>
2024-07-07 21:22:30 +0800

    refactor: Simplify Alert component code

--- src/components/(files)/display/Alert.tsx
+++ src/components/(files)/display/Alert.tsx
@@ -1,19 +1,9 @@
-import { Button } from "@mui/material";
 import { AlertM } from "@/components/Motion";
 import { yVar } from "@/components/MotionProps";
 
 export default function Alert() {
   return (
-    <AlertM
-      variants={yVar}
-      severity="error"
-      sx={{ width: 1 }}
-      action={
-        <Button color="inherit" size="small">
-          Sign In
-        </Button>
-      }
-    >
+    <AlertM variants={yVar} severity="error" sx={{ width: 1 }}>
       This page is for admin only. If your account is unauthorized, you will not
       be able to access even after completing the login process.
     </AlertM>

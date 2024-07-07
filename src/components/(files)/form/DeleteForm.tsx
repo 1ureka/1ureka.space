ed35a11147bf0dd2645b432f54284be9df42755b
1ureka <summerheadquarters001@gmail.com>
2024-07-07 23:02:35 +0800

    feat: Add delete UI

---
+++ src/components/(files)/form/DeleteForm.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import type { ImageMetadata } from "@/data/type";
+import { useRecoilState } from "recoil";
+import { FILES_SELECTED } from "@/context/store";
+
+import { useState } from "react";
+import toast from "react-hot-toast";
+import Link, { type LinkProps } from "next/link";
+import { useRouter } from "next/navigation";
+import { deleteImages } from "@/utils/server-actions";
+
+import { Button, Grid, IconButton, Dialog } from "@mui/material";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+
+import { BoxM, GridM, DialogTitleM } from "@/components/Motion";
+import { DialogActionsM, DialogContentM } from "@/components/Motion";
+import { DeleteField } from "..";
+
+interface DeleteFormProps {
+  open: boolean;
+  closeHref: LinkProps["href"];
+  metadataList: ImageMetadata[];
+}
+
+export default function DeleteForm({
+  open,
+  closeHref,
+  metadataList,
+}: DeleteFormProps) {
+  const router = useRouter();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const [selected, setSelected] = useRecoilState(FILES_SELECTED);
+  const selectedMetadata = metadataList.filter(({ name }) =>
+    selected.includes(name)
+  );
+
+  const handleSubmit = async () => {
+    if (isSubmitting) return;
+    setIsSubmitting(true);
+
+    toast.loading("Saving changes...", {
+      style: { minWidth: "20rem" },
+      id: "submit",
+    });
+
+    try {
+      const ids = selectedMetadata.map(({ id }) => id);
+      const result = await deleteImages(ids);
+      const error = result?.error ?? [];
+
+      if (error.length === 0) {
+        toast.success("Changes saved successfully!", { id: "submit" });
+        setSelected([]);
+
+        router.refresh();
+      } else {
+        toast.dismiss("submit");
+        error.map((message) => toast.error(message));
+      }
+    } catch {
+      toast.error("Something went wrong", { id: "submit" });
+    }
+
+    setIsSubmitting(false);
+  };
+
+  const createHandleCancel = (name: string) => () => {
+    setSelected((prev) => prev.filter((prevName) => prevName !== name));
+  };
+
+  return (
+    <Dialog
+      fullWidth
+      maxWidth="md"
+      open={open}
+      PaperProps={{ sx: { overflow: "hidden" } }}
+    >
+      <DialogTitleM layout>Delete Files</DialogTitleM>
+
+      <BoxM layout sx={{ position: "absolute", inset: "0 0 auto auto" }}>
+        <Link href={closeHref}>
+          <IconButton sx={{ mt: 1.5, mr: 1.5 }}>
+            <CloseRoundedIcon fontSize="small" sx={{ color: "grey.500" }} />
+          </IconButton>
+        </Link>
+      </BoxM>
+
+      <DialogContentM
+        sx={{ display: "grid", gap: 4, scrollbarGutter: "stable both-edges" }}
+        dividers
+        layout
+      >
+        <Grid container columns={2} spacing={3} minHeight={"20rem"}>
+          {selectedMetadata.map((metadata) => (
+            <GridM item layout xs={2} md={1} key={metadata.id}>
+              <BoxM initial="initial" animate="animate">
+                <DeleteField
+                  metadata={metadata}
+                  onRemove={createHandleCancel(metadata.name)}
+                />
+              </BoxM>
+            </GridM>
+          ))}
+        </Grid>
+      </DialogContentM>
+
+      <DialogActionsM layout>
+        <Button
+          disabled={selected.length === 0 || isSubmitting}
+          onClick={handleSubmit}
+        >
+          Save Change
+        </Button>
+      </DialogActionsM>
+    </Dialog>
+  );
+}
--- src/components/(files)/index.ts
+++ src/components/(files)/index.ts
@@ -1,6 +1,7 @@
 export { default as CategoryToggle } from "./input/CategoryToggle";
 export { default as FileDropField } from "./input/FileDropField";
 export { default as UploadField } from "./input/UploadField";
+export { default as DeleteField } from "./input/DeleteField";
 
 export { default as Alert } from "./display/Alert";
 export { default as TableHeader } from "./display/TableHeader";
@@ -9,6 +10,7 @@ export { default as TableBody } from "./display/TableBody";
 export { default as TableFooter } from "./display/TableFooter";
 
 export { default as UploadForm } from "./form/UploadForm";
+export { default as DeleteForm } from "./form/DeleteForm";
 
 export { default as Options } from "./block/Options";
 export { default as Table } from "./block/Table";

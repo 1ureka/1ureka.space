12d5e0e830b9f2c550271c9154db6b3c49b5b716
1ureka <summerheadquarters001@gmail.com>
2024-07-08 15:39:29 +0800

    feat: Refactor image upload and processing functionality

--- src/utils/server-actions.ts
+++ src/utils/server-actions.ts
@@ -1,12 +1,15 @@
 "use server";
 
-import { auth } from "@/auth";
+import sharp from "sharp";
 import { z } from "zod";
 import { createMetadataSchema, MetadataSchema } from "@/schema/schema";
 
-import sharp from "sharp";
+import { auth } from "@/auth";
 import { log } from "@/utils/server-utils";
-import { getMetadataNames } from "@/data/table";
+
+import { createMetadata, createOrigins, createThumbnails } from "@/data/table";
+import { getMetadataIDs, getMetadataNames } from "@/data/table";
+import { deleteMetadata } from "@/data/table";
 
 /**
  * 驗證相關的元數據並上傳圖片。
@@ -20,17 +23,16 @@ export async function uploadImages(
 
   try {
     const session = await auth();
-    if (!session)
+    if (!session) {
       return { error: ["Authentication required to upload files."] };
+    }
 
     const existingNames = await getMetadataNames();
     const schema = createMetadataSchema(existingNames);
     const result = schema.safeParse(data);
 
     if (!result.success) {
-      const { issues } = result.error;
-
-      const errorMessages = issues.map((issue) => {
+      const errorMessages = result.error.issues.map((issue) => {
         const { path, message } = issue;
         const index = path[1] as number;
         const field = path[2] as string;
@@ -54,7 +56,7 @@ export async function uploadImages(
       files.map((file) => file.arrayBuffer())
     );
 
-    const origins = await Promise.all(
+    const bufferO = await Promise.all(
       arrayBuffers.map((arrayBuffer) =>
         sharp(Buffer.from(arrayBuffer))
           .webp({ quality: 100, effort: 6 })
@@ -62,7 +64,7 @@ export async function uploadImages(
       )
     );
 
-    const thumbnails = await Promise.all(
+    const bufferT = await Promise.all(
       arrayBuffers.map((arrayBuffer) =>
         sharp(Buffer.from(arrayBuffer))
           .resize(480, 270, { fit: "cover" })
@@ -73,13 +75,27 @@ export async function uploadImages(
 
     const metadataList = data.fieldArray.map((metadata, i) => ({
       ...metadata,
-      size: origins[i].byteLength + thumbnails[i].byteLength,
+      size: bufferO[i].byteLength + bufferT[i].byteLength,
     }));
 
-    console.log(metadataList, thumbnails, origins);
+    const metadataIDs = await createMetadata(metadataList);
 
-    // TODO: 這裡應該要將資料寫入資料庫
+    const originList = bufferO.map((buffer, i) => ({
+      metadataId: metadataIDs[i],
+      bytes: buffer,
+    }));
+
+    const thumbnailList = bufferT.map((buffer, i) => ({
+      metadataId: metadataIDs[i],
+      bytes: buffer,
+    }));
+
+    await Promise.all([
+      createOrigins(originList),
+      createThumbnails(thumbnailList),
+    ]);
   } catch (error) {
+    console.error(`Error: ${error}`);
     return { error: ["Something went wrong"] };
   }
 
@@ -91,14 +107,37 @@ export async function uploadImages(
  * @returns 成功時回傳 undefined，失敗時回傳一個包含錯誤訊息的物件。
  */
 export async function updateImages(
-  data: z.infer<typeof MetadataSchema> & { fieldArray: { id: string }[] }
+  data: z.infer<typeof MetadataSchema>,
+  metadataIDs: string[]
 ) {
   log("ACTION", "Updating images");
 
   try {
     const session = await auth();
-    if (!session)
+    if (!session) {
       return { error: ["Authentication required to modify files."] };
+    }
+
+    const existingNames = await getMetadataNames();
+    const schema = createMetadataSchema(existingNames);
+    const result = schema.safeParse(data);
+
+    if (!result.success) {
+      const errorMessages = result.error.issues.map((issue) => {
+        const { path, message } = issue;
+        const index = path[1] as number;
+        const field = path[2] as string;
+        return `"${message}" in image ${index + 1} at field "${field}"`;
+      });
+
+      return { error: errorMessages };
+    }
+
+    const existingIds = await getMetadataIDs();
+    if (!metadataIDs.every((id) => existingIds.includes(id))) {
+      return { error: ["Some ids do not exist."] };
+    }
+
     // TODO
   } catch (error) {
     return { error: ["Something went wrong"] };
@@ -116,11 +155,16 @@ export async function deleteImages(ids: string[]) {
 
   try {
     const session = await auth();
-    if (!session)
+    if (!session) {
       return { error: ["Authentication required to delete files."] };
+    }
 
-    console.log(`Deleting images with ids: ${ids}`);
-    // TODO
+    const existingIds = await getMetadataIDs();
+    if (!ids.every((id) => existingIds.includes(id))) {
+      return { error: ["Some ids do not exist."] };
+    }
+
+    await deleteMetadata(ids);
   } catch (error) {
     return { error: ["Something went wrong"] };
   }

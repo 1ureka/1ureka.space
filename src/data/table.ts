41bbcab7dd139c8fac548dfc6bb530f5da982684
1ureka <summerheadquarters001@gmail.com>
2024-07-24 15:33:22 +0800

    refactor: Add functions for verifying metadata categories, thumbnails, and origins, and summarizing category sizes

--- src/data/table.ts
+++ src/data/table.ts
@@ -233,3 +233,73 @@ export async function updateMetadata(
     throw new Error(`Failed to update ImageMetadata`);
   }
 }
+
+export async function verifyAllCategory() {
+  log("DATABASE", `verify metadata category`);
+
+  try {
+    await dbAuth();
+
+    const count = await db.imageMetadata.count({
+      where: { NOT: { category: { in: ["scene", "props"] } } },
+    });
+
+    return count;
+  } catch (error) {
+    throw new Error(`Failed to verify metadata category`);
+  }
+}
+
+export async function verifyAllThumbnail() {
+  log("DATABASE", `verify thumbnails`);
+
+  try {
+    await dbAuth();
+
+    const count = await db.imageMetadata.count({
+      where: { thumbnail: null },
+    });
+
+    return count;
+  } catch (error) {
+    throw new Error(`Failed to verify thumbnail`);
+  }
+}
+
+export async function verifyAllOrigin() {
+  log("DATABASE", `verify origins`);
+
+  try {
+    await dbAuth();
+
+    const count = await db.imageMetadata.count({
+      where: { origin: null },
+    });
+
+    return count;
+  } catch (error) {
+    throw new Error(`Failed to verify origin`);
+  }
+}
+
+export async function summaryCategorySize() {
+  log("DATABASE", `summary category size`);
+
+  try {
+    await dbAuth();
+
+    const groupBy = await db.imageMetadata.groupBy({
+      by: ["category"],
+      _sum: { size: true },
+    });
+
+    const summary = groupBy.reduce((acc, { category, _sum }) => {
+      acc[category] = _sum.size ?? NaN;
+      return acc;
+    }, {} as Record<string, number>);
+
+    return summary;
+  } catch (error) {
+    throw new Error(`Failed to summary category size`);
+  }
+}

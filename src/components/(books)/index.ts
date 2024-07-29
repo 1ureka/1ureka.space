b656c7d77bc8dd31ae58a89ed57f3bf95b8ee47a
1ureka <summerheadquarters001@gmail.com>
2024-07-30 02:35:12 +0800

    refactor: Add Accordion component for filtering images in gallery and carousels

--- src/components/(books)/index.ts
+++ src/components/(books)/index.ts
@@ -4,3 +4,8 @@ export { default as Badge } from "./display/Badge";
 export { default as Illustration } from "./display/Illustration ";
 
 export { default as Gallery } from "./block/Gallery";
+
+import dynamic from "next/dynamic";
+export const Accordion = dynamic(() => import("./block/Accordion"), {
+  ssr: false,
+});

571a5d3fd4a0b7a05c64017976c2bb9632f51e39
1ureka <summerheadquarters001@gmail.com>
2024-07-29 14:16:08 +0800

    refactor: Update Setting component layout and styling for responsive design

--- src/components/(bookSpine)/block/Setting.tsx
+++ src/components/(bookSpine)/block/Setting.tsx
@@ -1,26 +1,16 @@
 import { AnimatePresence } from "framer-motion";
-import { Switch, useColorScheme } from "@mui/material";
+import { Switch, useColorScheme, useMediaQuery } from "@mui/material";
 import { List, ListItemIcon, ListItemText } from "@mui/material";
+import type { PaperProps, Theme } from "@mui/material";
 
 import WarningRoundedIcon from "@mui/icons-material/WarningRounded";
 import DarkModeRoundedIcon from "@mui/icons-material/DarkModeRounded";
 import DashboardCustomizeRoundedIcon from "@mui/icons-material/DashboardCustomizeRounded";
 
 import { AlertM, DividerM, ListItemM, PaperM } from "@/components/Motion";
-import { booksSpineExtandedVar, yScaleVar } from "@/components/MotionProps";
+import { createExtandedVar, yScaleVar } from "@/components/MotionProps";
 import { useRecoilState } from "recoil";
 import { BOOKS_IS_EXPANDED } from "@/context/store";
-
-const containerSx = {
-  position: "absolute",
-  bottom: -10,
-  left: "100%",
-  p: 3.5,
-  transformOrigin: "left",
-  borderRadius: "0 20px 0 0",
-  border: `solid 1px var(--mui-palette-divider)`,
-  borderWidth: "1px 1px 0px 1px",
-};
 
 export default function Setting({
   open,
@@ -29,12 +19,30 @@ export default function Setting({
   open: boolean;
   UserButton: React.ReactNode;
 }) {
+  const isMobile = useMediaQuery<Theme>((theme) =>
+    theme.breakpoints.down("sm")
+  );
+
+  const containerSx: PaperProps["sx"] = {
+    position: "absolute",
+    inset: isMobile ? "auto auto 100% auto" : "auto auto -10px 100%",
+
+    width: isMobile ? 1 : "auto",
+    p: 3.5,
+    transformOrigin: isMobile ? "bottom" : "left",
+
+    boxShadow: "none",
+    borderRadius: "0 20px 0 0",
+    borderLeft: isMobile ? "" : `solid 1px var(--mui-palette-divider)`,
+    borderBottom: isMobile ? `solid 1px var(--mui-palette-divider)` : "",
+  };
+
   return (
     <AnimatePresence>
       {open && (
         <PaperM
           sx={containerSx}
-          variants={booksSpineExtandedVar(0.08)}
+          variants={createExtandedVar(0.08, isMobile ? "y" : "x")}
           initial="initial"
           animate="animate"
           exit="initial"

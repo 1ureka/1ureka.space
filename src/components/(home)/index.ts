eb9892cc37670aba1acc47ffa64576fe257f3ee9
1ureka <summerheadquarters001@gmail.com>
2024-07-31 22:32:20 +0800

    feat: Add CardMedia1, CardMedia2, CardMedia3, CardMedia4, and CardMedia5 components for displaying card media with different shapes and styles

--- src/components/(home)/index.ts
+++ src/components/(home)/index.ts
@@ -1,1 +1,7 @@
-export { default as NavCard } from "./input/NavCard";
+export { default as Card } from "./display/Card";
+
+export { default as CardMedia1 } from "./svg/CardMedia1";
+export { default as CardMedia2 } from "./svg/CardMedia2";
+export { default as CardMedia3 } from "./svg/CardMedia3";
+export { default as CardMedia4 } from "./svg/CardMedia4";
+export { default as CardMedia5 } from "./svg/CardMedia5";

a3f76c40e3990142f4bbf7d641f34acf6b114983
1ureka <summerheadquarters001@gmail.com>
2024-08-03 08:14:17 +0800

    feat: Remove TitleBox, CreateButton components from explore page

--- src/components/(explore)/index.ts
+++ src/components/(explore)/index.ts
@@ -1,5 +1,2 @@
-export { default as TitleBox } from "./display/TitleBox";
-
 export { default as Carousels } from "./input/Carousels";
-export { default as CreateButton } from "./input/CreateButton";
 export { default as Indicator } from "./input/Indicator";

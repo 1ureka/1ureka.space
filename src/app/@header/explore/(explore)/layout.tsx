2ffc3de17f23d9cfcd8c0a067c4fa7360a7890df
1ureka <summerheadquarters001@gmail.com>
2024-08-03 08:16:56 +0800

    feat: Update explore page layout and styling

--- src/app/@header/explore/(explore)/layout.tsx
+++ src/app/@header/explore/(explore)/layout.tsx
@@ -1,27 +1,80 @@
+import { NextLinkComposed } from "@/components/Link";
 import { DividerM, StackM } from "@/components/Motion";
-import { layoutChildMotionProps } from "@/components/MotionProps";
+import { layoutChildMotionProps, yVar } from "@/components/MotionProps";
 import { yScaleVar } from "@/components/MotionProps";
 
-import { CreateButton, TitleBox } from "@/components/(explore)";
-import { Carousels, Indicator } from "@/components/(explore)";
+import { Box, Button, Slider } from "@mui/material";
+import MapRoundedIcon from "@mui/icons-material/MapRounded";
+import NoteAddRoundedIcon from "@mui/icons-material/NoteAddRounded";
+import VolumeUpRoundedIcon from "@mui/icons-material/VolumeUpRounded";
+import VolumeDownRoundedIcon from "@mui/icons-material/VolumeDownRounded";
 
-export default function ExploreHeaderLayout(_: { children: React.ReactNode }) {
+export default function ExploreHeaderLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <StackM
       {...layoutChildMotionProps()}
-      direction={{ sm: "row" }}
-      alignItems={{ sm: "flex-end" }}
+      direction="row"
+      alignItems="center"
       gap={{ xs: 1, sm: 3 }}
-      sx={{ py: 3, px: { xs: 2, sm: 7 } }}
+      sx={{ px: { xs: 2, sm: 7 }, pb: 1, mt: -3, color: "text.secondary" }}
     >
-      <TitleBox />
+      <TitleIcon />
       <DividerM variants={yScaleVar} orientation="vertical" flexItem />
 
-      <Carousels amount={10} sx={{ flexGrow: 1 }} />
-      <Indicator amount={10} />
-      <DividerM variants={yScaleVar} orientation="vertical" flexItem />
+      <StackM variants={yScaleVar}>
+        <Button
+          component={NextLinkComposed}
+          to={`/explore/new`}
+          startIcon={<NoteAddRoundedIcon />}
+          variant="outlined"
+          disableElevation
+          color="inherit"
+        >
+          Create New
+        </Button>
+      </StackM>
 
-      <CreateButton />
+      <StackM variants={yScaleVar}>{children}</StackM>
+
+      <Box sx={{ flexGrow: 1 }} />
+
+      <StackM
+        variants={yVar}
+        spacing={2}
+        direction="row"
+        alignItems="center"
+        sx={{ width: 200 }}
+      >
+        <VolumeDownRoundedIcon />
+        <Slider aria-label="Volume" size="small" />
+        <VolumeUpRoundedIcon />
+      </StackM>
+    </StackM>
+  );
+}
+
+function TitleIcon() {
+  return (
+    <StackM
+      variants={yScaleVar}
+      sx={{ position: "relative", overflow: "hidden", borderRadius: "999px" }}
+    >
+      <Box
+        sx={{
+          position: "absolute",
+          inset: 0,
+          bgcolor: "text.secondary",
+          opacity: 0.3,
+          zIndex: -1,
+        }}
+      />
+      <MapRoundedIcon
+        sx={{ fontSize: "h4.fontSize", color: "text.secondary", m: 1 }}
+      />
     </StackM>
   );
 }

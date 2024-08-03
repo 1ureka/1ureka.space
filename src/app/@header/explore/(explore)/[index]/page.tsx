74567c381659ddccc0e86447b1bd9efd9b7d2c9e
1ureka <summerheadquarters001@gmail.com>
2024-08-03 08:17:05 +0800

    feat: Add edit button to explore page for each item

--- src/app/@header/explore/(explore)/[index]/page.tsx
+++ src/app/@header/explore/(explore)/[index]/page.tsx
@@ -1,3 +1,23 @@
-export default function Header() {
-  return <></>;
+import { NextLinkComposed } from "@/components/Link";
+import { Button } from "@mui/material";
+import BorderColorRoundedIcon from "@mui/icons-material/BorderColorRounded";
+
+export default function Page({
+  params: { index },
+}: {
+  params: { index: string };
+}) {
+  const id = `${index}'s id`;
+
+  return (
+    <Button
+      component={NextLinkComposed}
+      to={`/explore/edit/${id}`}
+      startIcon={<BorderColorRoundedIcon />}
+      variant="outlined"
+      color="inherit"
+    >
+      Edit
+    </Button>
+  );
 }

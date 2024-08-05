32e7990eed61a8f902ac9b9565ccfd7dddcc5c22
1ureka <summerheadquarters001@gmail.com>
2024-08-05 11:13:44 +0800

    feat: Add Point component to explore page

--- src/components/(explore)/index.ts
+++ src/components/(explore)/index.ts
@@ -1,4 +1,5 @@
 export { default as Carousels } from "./input/Carousels";
 export { default as Indicator } from "./input/Indicator";
+export { default as Point } from "./input/Point";
 
 export { default as ExploreForm } from "./form/ExploreForm";

f954cc53ea9f8888b6401c7b9a104229d5d15e59
1ureka <summerheadquarters001@gmail.com>
2024-08-05 11:20:50 +0800

    feat: Add UnAuth page component

---
+++ src/app/(content)/unAuth/page.tsx
@@ -0,0 +1,33 @@
+import { NextLinkComposed } from "@/components/Link";
+import { BoxM, StackM } from "@/components/Motion";
+import { layoutChildMotionProps, yScaleVar } from "@/components/MotionProps";
+import { Button, Typography } from "@mui/material";
+import ErrorRoundedIcon from "@mui/icons-material/ErrorRounded";
+
+export default function UnAuth() {
+  return (
+    <StackM
+      {...layoutChildMotionProps()}
+      spacing={3}
+      alignItems={"center"}
+      sx={{ maxWidth: 675, m: "auto", p: 10 }}
+    >
+      <BoxM variants={yScaleVar}>
+        <ErrorRoundedIcon fontSize="large" color="primary" />
+      </BoxM>
+
+      <StackM variants={yScaleVar} spacing={1} alignItems={"center"}>
+        <Typography variant="h6">Unauthorized access</Typography>
+        <Typography variant="body2" className="text-ellipsis">
+          401: You are not authorized to access this page.
+        </Typography>
+      </StackM>
+
+      <BoxM variants={yScaleVar}>
+        <Button component={NextLinkComposed} to="/">
+          Home
+        </Button>
+      </BoxM>
+    </StackM>
+  );
+}

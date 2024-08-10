1b2795e053e3e20b3438e687abefcedf79a071b7
1ureka <summerheadquarters001@gmail.com>
2024-08-10 17:25:00 +0800

    refactor: Update Loading component to use MUI Skeleton for better loading experience

--- src/app/(content)/explore/view/@intro/[index]/loading.tsx
+++ src/app/(content)/explore/view/@intro/[index]/loading.tsx
@@ -1,3 +1,33 @@
+import { Stack, Typography, Skeleton } from "@mui/material";
+
 export default function Loading() {
-  return <p>Loading...</p>;
+  return (
+    <Stack gap={0.5}>
+      <Skeleton variant="text" animation="wave">
+        <Typography variant="h4">Explore Name</Typography>
+      </Skeleton>
+
+      <Skeleton variant="text" animation="wave">
+        <Typography variant="subtitle2" sx={{ fontStyle: "italic" }}>
+          From "Artwork"
+        </Typography>
+      </Skeleton>
+
+      <Skeleton
+        variant="rounded"
+        animation="wave"
+        sx={{ width: 1, height: "0.75rem", mt: 1.5 }}
+      />
+      <Skeleton
+        variant="rounded"
+        animation="wave"
+        sx={{ width: 1, height: "0.75rem", mt: 1 }}
+      />
+      <Skeleton
+        variant="rounded"
+        animation="wave"
+        sx={{ width: 0.6, height: "0.75rem", mt: 1 }}
+      />
+    </Stack>
+  );
 }

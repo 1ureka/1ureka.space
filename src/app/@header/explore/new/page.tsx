6bac465e8c0d7affc285212c38d52b6fae297d30
1ureka <summerheadquarters001@gmail.com>
2024-08-10 12:59:25 +0800

    refactor: Remove unused code and update import paths in header components

--- src/app/@header/explore/new/page.tsx
+++ src/app/@header/explore/new/page.tsx
@@ -1,34 +1,3 @@
-import { NextLinkComposed } from "@/components/Link";
-import { StackM } from "@/components/Motion";
-import { yScaleVar } from "@/components/MotionProps";
-
-import { auth } from "@/auth";
-import { Button } from "@mui/material";
-import ArrowBackIosNewRoundedIcon from "@mui/icons-material/ArrowBackIosNewRounded";
-
-export default async function CreateHeader() {
-  const session = await auth();
-  const isAuth =
-    !!session && JSON.stringify(session.user.id) === process.env.ALLOWED_USER;
-
-  if (!isAuth) {
-    return null;
-  }
-
-  return (
-    <>
-      <StackM variants={yScaleVar}>
-        <Button
-          component={NextLinkComposed}
-          to={`/explore/view/0`}
-          startIcon={<ArrowBackIosNewRoundedIcon />}
-          variant="outlined"
-          disableElevation
-          color="inherit"
-        >
-          Go Back
-        </Button>
-      </StackM>
-    </>
-  );
+export default function Header() {
+  return null;
 }
--- src/app/@header/explore/view/[index]/page.tsx
+++ src/app/@header/explore/view/[index]/page.tsx
@@ -1,24 +1,3 @@
-import { NextLinkComposed } from "@/components/Link";
-import { isValidIndex } from "@/utils/utils";
-
-import { Button } from "@mui/material";
-import BorderColorRoundedIcon from "@mui/icons-material/BorderColorRounded";
-
-export default function Page({ params }: { params: { index: string } }) {
-  const index = isValidIndex(params.index, 10);
-  if (index === -1) return null;
-
-  const id = `${index}'s id`;
-
-  return (
-    <Button
-      component={NextLinkComposed}
-      to={`/explore/edit/${id}`}
-      startIcon={<BorderColorRoundedIcon />}
-      variant="outlined"
-      color="inherit"
-    >
-      Edit
-    </Button>
-  );
+export default function Header() {
+  return null;
 }

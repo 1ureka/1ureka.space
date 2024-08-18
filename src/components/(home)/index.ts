27102ddfa6335f1dc666e9be1ec4c93dddfe42fe
1ureka <summerheadquarters001@gmail.com>
2024-08-19 07:31:36 +0800

    refactor: Update CardMedia imports to use media folder

--- src/components/(home)/index.ts
+++ src/components/(home)/index.ts
@@ -1,8 +1,8 @@
 export { default as Card } from "./display/Card";
 export { default as Chart } from "./display/Chart";
 
-export { default as CardMedia1 } from "./svg/CardMedia1";
-export { default as CardMedia2 } from "./svg/CardMedia2";
-export { default as CardMedia3 } from "./svg/CardMedia3";
-export { default as CardMedia4 } from "./svg/CardMedia4";
-export { default as CardMedia5 } from "./svg/CardMedia5";
+export { default as CardMedia1 } from "./media/CardMedia1";
+export { default as CardMedia2 } from "./media/CardMedia2";
+export { default as CardMedia3 } from "./media/CardMedia3";
+export { default as CardMedia4 } from "./media/CardMedia4";
+export { default as CardMedia5 } from "./media/CardMedia5";

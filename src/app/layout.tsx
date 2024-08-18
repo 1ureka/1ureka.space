87a3d06840f5d278bf85c5ea5a01a7411681379c
1ureka <summerheadquarters001@gmail.com>
2024-08-19 05:16:30 +0800

    feat: Remove unused components, update file structure, and add Docker component for Docker integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
+import { Toaster } from "react-hot-toast";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
-import { CssBaseline } from "@mui/material";
+import { Box, CssBaseline } from "@mui/material";
 import "@/app/index.css";
 
 import ContextProvider from "@/context/ContextProvider";
 import ThemeProvider from "@/theme/ThemeProvider";
-import Frame from "./frame";
-import UserButton from "@/components/(auth)/UserButton";
+import Docker from "@/components/(docker)";
 
 export const metadata: Metadata = {
   title: {
@@ -16,6 +16,50 @@ export const metadata: Metadata = {
   description:
     "A personal website for storing and managing a portfolio of 3D CG, with basic image editing capabilities.",
 };
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <Box
+      sx={{
+        display: "grid",
+        gridTemplateColumns: "auto 1fr",
+        height: 1,
+        bgcolor: "content.layer2",
+      }}
+    >
+      <Docker />
+
+      <Box
+        component="main"
+        sx={{
+          position: "relative",
+          height: 1,
+          px: { xs: 6, sm: 9, md: 12, lg: 15, xl: 18 },
+          py: { xs: 1, sm: 3, md: 5, lg: 7 },
+          overflowY: "auto",
+          scrollbarGutter: "stable",
+        }}
+      >
+        <Box id="portal-root" sx={{ position: "absolute", inset: 0 }} />
+        <Box sx={{ position: "relative", minHeight: 1 }}>{children}</Box>
+      </Box>
+    </Box>
+  );
+}
+
+function MuiToaster() {
+  return (
+    <Toaster
+      toastOptions={{
+        className: "",
+        style: {
+          background: "var(--mui-palette-SnackbarContent-bg)",
+          color: "var(--mui-palette-SnackbarContent-color)",
+        },
+      }}
+    />
+  );
+}
 
 export default function RootLayout({
   header,
@@ -30,12 +74,11 @@ export default function RootLayout({
         <AppRouterCacheProvider>
           <ThemeProvider>
             <CssBaseline />
+
             <ContextProvider>
-              <Frame
-                header={header}
-                content={children}
-                UserButton={<UserButton />}
-              />
+              <Layout>{children}</Layout>
+
+              <MuiToaster />
             </ContextProvider>
           </ThemeProvider>
         </AppRouterCacheProvider>

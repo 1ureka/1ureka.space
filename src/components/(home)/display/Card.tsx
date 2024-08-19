4a6c8a67a04afee6a7fde1a5c6732899f5373778
1ureka <summerheadquarters001@gmail.com>
2024-08-19 18:01:36 +0800

    refactor: Update Card component to optimize code readibility

--- src/components/(home)/display/Card.tsx
+++ src/components/(home)/display/Card.tsx
@@ -1,13 +1,12 @@
 import "server-only";
 
-import { ButtonBase, Typography } from "@mui/material";
-import { Box, Stack } from "@mui/material";
-import type { TypographyProps as TP } from "@mui/material";
+import Link from "next/link";
+import { Box, Stack, ButtonBase, Typography } from "@mui/material";
 import ArrowCircleRightRoundedIcon from "@mui/icons-material/ArrowCircleRightRounded";
 
+import { createPRNG } from "@/utils/utils";
 import { BoxM, StackM, TypographyM } from "@/components/Motion";
 import Block from "@/components/Block";
-import Link from "next/link";
 
 type CardProps = {
   variant: "contained" | "outlined";
@@ -22,88 +21,49 @@ type CardProps = {
   sx?: React.CSSProperties;
 };
 
-const hex: Record<CardProps["color"], string> = {
-  primary: "#e783ad",
-  secondary: "#83e7bd",
+const colorMap: Record<CardProps["color"], { main: string; hover: string }> = {
+  primary: { main: "#e783ad", hover: "#e783ad" },
+  secondary: { main: "#71C8A4", hover: "#83e7bd" },
 };
 
-function createPRNG(seed: number) {
-  let x = seed;
+const hoverSx = (
+  color: CardProps["color"]
+): React.ComponentProps<typeof Box>["sx"] => ({
+  // self
+  "& > *": {
+    transition: "all 0.25s ease",
+  },
+  "&:hover > *": {
+    scale: "1.02",
+  },
+  // shadow
+  "& > div:first-child": {
+    outline: `0px solid ${colorMap[color].hover}00`,
+  },
+  "&:hover > div:first-child": {
+    outline: `7.5px solid ${colorMap[color].hover}35`,
+  },
+  // media
+  "& .card-media > *": {
+    transition: "scale 0.25s ease",
+    scale: "1.001",
+  },
+  "&:hover .card-media > *": {
+    scale: "1.1",
+  },
+});
 
-  // 線性同餘生成器 (Linear Congruential Generator, LCG) 是一個簡單的 PRNG 實現
-  function next() {
-    x = (x * 1664525 + 1013904223) % 4294967296;
-    return x / 4294967296; // 將結果縮放到 [0, 1) 之間
-  }
+export default function Card(props: CardProps) {
+  const { variant, color, href, decoration = "both", sx, ...rest } = props;
+  const { media, icon, title, subTitle, description } = rest;
 
-  return next;
-}
-
-function createHoverTypo(variant: TP["variant"], text: string, color: string) {
-  const prng = createPRNG(text.length);
-
-  return (
-    <BoxM
-      variants={{ hover: { y: -30 } }}
-      transition={{ type: "spring", delayChildren: 0.2 }}
-    >
-      {text.split("").map((l, i) => (
-        <TypographyM
-          key={i}
-          variant={variant}
-          sx={{ color, display: "inline-block" }}
-          variants={{ hover: { y: 30 } }}
-          transition={{ type: "spring", bounce: 0, delay: prng() * 0.5 }}
-        >
-          {l}
-        </TypographyM>
-      ))}
-    </BoxM>
-  );
-}
-
-export default function Card({
-  variant,
-  color,
-  href,
-  decoration = "both",
-  media,
-  icon,
-  title,
-  subTitle,
-  description,
-  sx,
-}: CardProps) {
   return (
     <Link href={href} style={sx}>
       <Block
         variant={variant}
-        color={`${color}.main`}
+        color={colorMap[color].main}
         decoration={decoration}
-        sx={{
-          width: 1,
-          height: 1,
-
-          "& > *": {
-            transition: "all 0.25s ease",
-          },
-          "&:hover > *": {
-            scale: "1.02",
-          },
-          "& > div:first-child": {
-            outline: `0px solid ${hex[color]}00`,
-          },
-          "&:hover > div:first-child": {
-            outline: `7.5px solid ${hex[color]}35`,
-          },
-          "& .card-media > *": {
-            transition: "scale 0.25s ease",
-            scale: "1.001",
-          },
-          "&:hover .card-media > *": {
-            scale: "1.1",
-          },
-        }}
+        sx={{ width: 1, height: 1, ...hoverSx(color) }}
         SlotProps={{
           childContainer: {
             whileHover: "hover",
@@ -124,37 +84,34 @@ export default function Card({
         )}
 
         <Box
-          sx={{
-            position: "relative",
-            height: 1,
-            minHeight: 200,
-            display: "grid",
-            alignItems: "end",
-          }}
+          position="relative"
+          sx={{ height: 1, minHeight: 200, display: "grid", alignItems: "end" }}
         >
           <Stack gap={1}>
             <Stack direction="row" alignItems="flex-end" gap={1}>
               {icon}
-              {createHoverTypo(
-                "h4",
-                `${title} `,
-                variant === "outlined" ? `${color}.main` : "text.primary"
-              )}
-              {createHoverTypo("subtitle2", `${subTitle} `, "text.secondary")}
+
+              <HoverTypo
+                text={`${title} `}
+                variant="h4"
+                color={
+                  variant === "outlined" ? colorMap[color].main : "text.primary"
+                }
+              />
+
+              <HoverTypo
+                text={`${subTitle} `}
+                variant="subtitle2"
+                color="text.secondary"
+              />
             </Stack>
 
-            <Typography variant="body2" sx={{ textAlign: "start" }}>
-              {description}
-            </Typography>
+            <Typography variant="body2">{description}</Typography>
 
             <StackM
               direction="row"
-              gap={1}
-              alignItems="center"
-              sx={{ opacity: 0, height: 0 }}
-              variants={{
-                hover: { opacity: 1, height: "auto" },
-              }}
+              sx={{ opacity: 0, height: 0, alignItems: "center", gap: 1 }}
+              variants={{ hover: { opacity: 1, height: "auto" } }}
             >
               <Typography variant="body2">LEARN MORE</Typography>
               <ArrowCircleRightRoundedIcon />
@@ -167,5 +124,36 @@ export default function Card({
         />
       </Block>
     </Link>
+  );
+}
+
+function HoverTypo({
+  variant,
+  text,
+  color,
+}: {
+  variant: React.ComponentProps<typeof Typography>["variant"];
+  text: string;
+  color: string;
+}) {
+  const prng = createPRNG(text.length);
+
+  return (
+    <BoxM
+      variants={{ hover: { y: -30 } }}
+      transition={{ type: "spring", delayChildren: 0.2 }}
+    >
+      {text.split("").map((l, i) => (
+        <TypographyM
+          key={i}
+          variant={variant}
+          sx={{ color, display: "inline-block" }}
+          variants={{ hover: { y: 30 } }}
+          transition={{ type: "spring", bounce: 0, delay: prng() * 0.5 }}
+        >
+          {l}
+        </TypographyM>
+      ))}
+    </BoxM>
   );
 }

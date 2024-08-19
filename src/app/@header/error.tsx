6ac5d1d71cffc26ebf1fb9aaa094cf1e721de313
1ureka <summerheadquarters001@gmail.com>
2024-08-20 04:08:54 +0800

    refactor: Update header components to remove unnecessary code

--- src/app/@header/error.tsx
+++ src/app/@header/error.tsx
@@ -1,37 +1,5 @@
 "use client";
 
-import { BoxM, StackM } from "@/components/Motion";
-import { createMotionProps, yScaleVar } from "@/components/MotionProps";
-import { IconButton, Typography } from "@mui/material";
-import ReplayRoundedIcon from "@mui/icons-material/ReplayRounded";
-
-export default function Error({
-  error,
-  reset,
-}: {
-  error: Error & { digest?: string };
-  reset: () => void;
-}) {
-  return (
-    <BoxM
-      {...createMotionProps()}
-      sx={{ display: "grid", placeItems: "center" }}
-    >
-      <StackM
-        variants={yScaleVar}
-        spacing={1}
-        direction={"row"}
-        alignItems={"center"}
-        justifyContent={"center"}
-        sx={{ p: 1, maxWidth: 675 }}
-      >
-        <Typography variant="body2" className="text-ellipsis">
-          {error.message}
-        </Typography>
-        <IconButton onClick={() => reset()}>
-          <ReplayRoundedIcon fontSize="small" color="primary" />
-        </IconButton>
-      </StackM>
-    </BoxM>
-  );
+export default function Error() {
+  return null;
 }
--- src/app/@header/loading.tsx
+++ src/app/@header/loading.tsx
@@ -1,9 +1,3 @@
-import { Box, LinearProgress } from "@mui/material";
-
 export default function Loading() {
-  return (
-    <Box sx={{ pt: 6 }}>
-      <LinearProgress />
-    </Box>
-  );
+  return null;
 }

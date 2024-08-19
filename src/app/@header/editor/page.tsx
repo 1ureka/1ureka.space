bd7aa6079e0f82b7ad6806956b21d0881b1b5ee1
1ureka <summerheadquarters001@gmail.com>
2024-08-20 03:25:23 +0800

    refactor: Update EditorContent component to improve layout and add image editor functionality

--- src/app/@header/editor/page.tsx
+++ src/app/@header/editor/page.tsx
@@ -1,34 +1,3 @@
-import { BoxM, DividerM, StackM } from "@/components/Motion";
-import { createMotionProps } from "@/components/MotionProps";
-import { yScaleVar, yVar } from "@/components/MotionProps";
-import { Typography } from "@mui/material";
-
-const text = {
-  title: "Image Editor",
-  info: `A toolkit featuring image conversion, compression, 
-    and filtering. It supports batch processing and includes before-and-after comparison.`,
-};
-
 export default function Header() {
-  return (
-    <StackM
-      {...createMotionProps()}
-      direction={{ sm: "row" }}
-      alignItems={{ sm: "flex-end" }}
-      gap={{ xs: 1, sm: 3 }}
-      sx={{ py: 3, px: { xs: 3, sm: 9 } }}
-    >
-      <BoxM variants={yScaleVar}>
-        <Typography variant="h6" sx={{ whiteSpace: { md: "nowrap" } }}>
-          {text.title}
-        </Typography>
-      </BoxM>
-
-      <DividerM variants={yScaleVar} orientation="vertical" flexItem />
-
-      <BoxM variants={yVar}>
-        <Typography variant="body2">{text.info}</Typography>
-      </BoxM>
-    </StackM>
-  );
+  return null;
 }

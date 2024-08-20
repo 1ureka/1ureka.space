dbfb813a396884572589e5608e3840ea5f798a9c
1ureka <summerheadquarters001@gmail.com>
2024-08-20 23:28:39 +0800

    refactor: Update UserButton component to use Button and Typography components

--- src/components/(settings)/input/UserButton.tsx
+++ src/components/(settings)/input/UserButton.tsx
@@ -1,11 +1,6 @@
-import { Avatar, IconButton } from "@mui/material";
-import { ListItemIcon, ListItemText } from "@mui/material";
-
+import { Avatar, Button, Typography } from "@mui/material";
 import LoginRoundedIcon from "@mui/icons-material/LoginRounded";
 import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
-
-import { ListItemM } from "@/components/Motion";
-import { yScaleVar } from "@/components/MotionProps";
 import { auth, signIn, signOut } from "@/auth";
 
 export default async function UserButton() {
@@ -13,34 +8,47 @@ export default async function UserButton() {
 
   async function login() {
     "use server";
-    await signIn("github", { redirectTo: "/?success=signIn" });
+    await signIn("github", { redirectTo: "/settings/?success=signIn" });
   }
 
   async function logout() {
     "use server";
-    await signOut({ redirectTo: "/?success=signOut" });
+    await signOut({ redirectTo: "/settings/?success=signOut" });
   }
 
   return (
-    <ListItemM variants={yScaleVar}>
-      <ListItemIcon>
-        <Avatar
-          sx={{ width: 24, height: 24 }}
-          src={(session && session.user?.image) ?? undefined}
-        />
-      </ListItemIcon>
+    <>
+      <Avatar
+        src={(session && session.user?.image) ?? undefined}
+        sx={{
+          border: "2px solid",
+          borderColor: "primary.main",
+          width: 36,
+          height: 36,
+        }}
+      />
 
-      <ListItemText primary={session ? session.user?.name : "Guest"} />
+      <Typography sx={{ fontSize: "0.8rem" }}>
+        {session
+          ? `You are currently signed in as ${session.user?.name}`
+          : "You are currently in guest mode"}
+      </Typography>
 
       <form action={session ? logout : login}>
-        <IconButton size="small" type="submit">
-          {session ? (
-            <LogoutRoundedIcon fontSize="small" />
-          ) : (
-            <LoginRoundedIcon fontSize="small" />
-          )}
-        </IconButton>
+        <Button
+          variant="contained"
+          startIcon={session ? <LogoutRoundedIcon /> : <LoginRoundedIcon />}
+          type="submit"
+          sx={{
+            scale: "1.001",
+            transition: "all 0.25s ease",
+            "&:hover": { scale: "1.02", bgcolor: "primary.light" },
+            "&:active": { scale: "0.97" },
+          }}
+        >
+          {session ? "Sign out" : "Sign in"}
+        </Button>
       </form>
-    </ListItemM>
+    </>
   );
 }

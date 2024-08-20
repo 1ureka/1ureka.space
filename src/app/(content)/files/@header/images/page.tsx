6976001b39bd2bd406a24d2a5abf4e88d47d9b88
1ureka <summerheadquarters001@gmail.com>
2024-08-21 05:14:37 +0800

    refactor: Replace old Header component from files page

--- src/app/(content)/files/@header/images/page.tsx
+++ src/app/(content)/files/@header/images/page.tsx
@@ -1,3 +1,49 @@
-export default function Page() {
-  return null;
+import { Button, Stack, Typography } from "@mui/material";
+import AddToPhotosRoundedIcon from "@mui/icons-material/AddToPhotosRounded";
+import ImageSearchRoundedIcon from "@mui/icons-material/ImageSearchRounded";
+import Link from "next/link";
+
+import { validateUserSession } from "@/auth";
+import { StackM } from "@/components/Motion";
+import { createMotionVar } from "@/components/MotionProps";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  await validateUserSession();
+
+  const { category: categoryParam } = searchParams;
+  const category =
+    categoryParam === "scene" || categoryParam === "props"
+      ? categoryParam
+      : "scene";
+
+  return (
+    <StackM variants={createMotionVar()} gap={1}>
+      <Typography variant="subtitle2">OPERATION:</Typography>
+
+      <Stack direction="row" gap={1.5}>
+        <Button
+          startIcon={<AddToPhotosRoundedIcon fontSize="small" />}
+          variant="contained"
+          disableElevation
+          component={Link}
+          href={`/files/images?category=${category}&form=upload`}
+        >
+          Add Image
+        </Button>
+
+        <Button
+          startIcon={<ImageSearchRoundedIcon fontSize="small" />}
+          variant="outlined"
+          component={Link}
+          href={`/files/images?category=${category}&form=verify`}
+        >
+          Verify Integrity
+        </Button>
+      </Stack>
+    </StackM>
+  );
 }

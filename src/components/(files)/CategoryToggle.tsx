05d01c00a3de3f6865ca7aadc6a4752df54b24f5
1ureka <summerheadquarters001@gmail.com>
2024-08-22 05:59:42 +0800

    refactor: Update import statements for next/link in CategoryToggle and TableHeader components

--- src/components/(files)/CategoryToggle.tsx
+++ src/components/(files)/CategoryToggle.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Link } from "next-view-transitions";
+import Link from "next/link";
 import { ToggleButton, ToggleButtonGroup } from "@mui/material";
 import { usePathname } from "next/navigation";
 

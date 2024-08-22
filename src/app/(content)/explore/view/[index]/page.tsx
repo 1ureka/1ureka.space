e574e181311eb8991552329e4401c9255b7fdbee
1ureka <summerheadquarters001@gmail.com>
2024-08-22 11:34:05 +0800

    refactor: Update page component to include fullscreen functionality

--- src/app/(content)/explore/view/[index]/page.tsx
+++ src/app/(content)/explore/view/[index]/page.tsx
@@ -1,25 +1,29 @@
+import { validateUserSession } from "@/auth";
 import { isValidIndex } from "@/utils/utils";
 import { delay } from "@/utils/server-utils";
-import { notFound } from "next/navigation";
 
-import { Skeleton } from "@mui/material";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import { Backdrop, Button, IconButton, Portal, Skeleton } from "@mui/material";
+import FullscreenRoundedIcon from "@mui/icons-material/FullscreenRounded";
+import FullscreenExitRoundedIcon from "@mui/icons-material/FullscreenExitRounded";
+
 import { BoxM } from "@/components/Motion";
 import { createMotionVar } from "@/components/MotionProps";
-import { validateUserSession } from "@/auth";
 
 export default async function Page({
   params: { index: indexString },
-}: // searchParams,
-{
+  searchParams,
+}: {
   params: { index: unknown };
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   const session = await validateUserSession({ isRedirect: false });
 
-  if (!session) return null;
-
   const index = isValidIndex(indexString, 10);
   if (index === -1) notFound();
+
+  const isFullscreen = typeof searchParams.fullscreen === "string";
 
   await delay(Math.random() * 2000);
 
@@ -33,6 +37,55 @@ export default async function Page({
         animation="wave"
         sx={{ height: 1, width: 1 }}
       />
+
+      {session && (
+        <Button
+          component={Link}
+          href={`/explore/view/${index}?fullscreen`}
+          color="primary"
+          startIcon={<FullscreenRoundedIcon />}
+          variant="outlined"
+          sx={{
+            position: "absolute",
+            inset: "auto 50% 0 auto",
+            translate: "50% 0",
+            m: 1,
+            "&:hover": { scale: "1.05" },
+            "&:active": { scale: "0.97" },
+            transition: "all 0.2s ease",
+            lineHeight: 1,
+          }}
+        >
+          Fullscreen
+        </Button>
+      )}
+
+      {session && (
+        <Portal>
+          <Backdrop open={isFullscreen}>
+            <Skeleton
+              variant="rectangular"
+              animation="wave"
+              sx={{ height: 1, width: 1 }}
+            />
+
+            <IconButton
+              component={Link}
+              href={`/explore/view/${index}`}
+              sx={{
+                position: "absolute",
+                inset: "auto 0 0 auto",
+                m: 1,
+                "&:hover": { scale: "1.05" },
+                "&:active": { scale: "0.97" },
+                transition: "all 0.2s ease",
+              }}
+            >
+              <FullscreenExitRoundedIcon />
+            </IconButton>
+          </Backdrop>
+        </Portal>
+      )}
     </BoxM>
   );
 }

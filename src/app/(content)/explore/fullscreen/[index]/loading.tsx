545913d6e5f3077bfe5ae094091bfaf93edf3a39
1ureka <summerheadquarters001@gmail.com>
2024-09-06 02:30:31 +0800

    refactor: Remove old explore-related pages and components

--- src/app/(content)/explore/fullscreen/[index]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { CircularProgress } from "@mui/material";
-
-export default function Loading() {
-  return (
-    <div style={{ alignSelf: "center", justifySelf: "center" }}>
-      <CircularProgress />
-    </div>
-  );
-}
--- src/app/(content)/explore/view/[index]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { LinearProgress, Stack } from "@mui/material";
-
-export default function Loading() {
-  return (
-    <Stack justifyContent="flex-end" sx={{ position: "relative", height: 1 }}>
-      <LinearProgress />
-    </Stack>
-  );
-}

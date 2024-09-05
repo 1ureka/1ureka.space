017cf1b5068dc931efddb6473e2850545bf6dd23
1ureka <summerheadquarters001@gmail.com>
2024-09-06 02:39:28 +0800

    feat: Add Intro component for explore page

---
+++ src/components/(explore)/Intro.tsx
@@ -0,0 +1,63 @@
+import { Box, IconButton, Tooltip, Typography } from "@mui/material";
+import ShuffleRoundedIcon from "@mui/icons-material/ShuffleRounded";
+import Block from "@/components/Block";
+import DropShadowContainer from "./display/DropShadowContainer";
+
+export default function Intro({
+  sx,
+  intro,
+  tags,
+}: {
+  sx?: React.ComponentProps<typeof Box>["sx"];
+  intro: React.ReactNode;
+  tags: React.ReactNode;
+}) {
+  return (
+    <DropShadowContainer
+      sx={{ display: "grid", gridTemplateRows: "1fr 1fr", gap: 3, ...sx }}
+    >
+      <Block
+        color="primary.main"
+        variant="contained"
+        decoration="right"
+        SlotProps={{
+          childContainer: { "data-mui-color-scheme": "dark" },
+        }}
+      >
+        {intro}
+      </Block>
+
+      <Block decoration="right">
+        <Box
+          sx={{ position: "relative", display: "flex", alignItems: "center" }}
+        >
+          <Typography variant="button" sx={{ pl: 1 }}>
+            tags
+          </Typography>
+
+          <Tooltip title="shuffle tags" arrow>
+            <IconButton
+              size="small"
+              sx={{
+                position: "absolute",
+                right: 0,
+                scale: "1.001",
+                transition: "all 0.15s ease",
+                "&:hover": { scale: "1.1" },
+                "&:active": { scale: "0.97" },
+              }}
+            >
+              <ShuffleRoundedIcon fontSize="small" />
+            </IconButton>
+          </Tooltip>
+        </Box>
+
+        <Typography variant="caption" sx={{ pl: 1 }}>
+          discover different camera angles, lighting and more . . .
+        </Typography>
+
+        {tags}
+      </Block>
+    </DropShadowContainer>
+  );
+}

15be60e6e73fc0d58679a02ed04e975f3819d440
1ureka <summerheadquarters001@gmail.com>
2024-09-06 03:48:05 +0800

    feat: Add create, edit, and table pages for projects in the files header

---
+++ src/app/(content)/files/@header/projects/create/page.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { Button, Stack } from "@mui/material";
+import ArrowBackIosNewRoundedIcon from "@mui/icons-material/ArrowBackIosNewRounded";
+
+import { validateSession } from "@/auth";
+import { BoxM } from "@/components/Motion";
+import { createMotionVar } from "@/components/MotionProps";
+
+export default async function Page() {
+  await validateSession();
+
+  return (
+    <Stack direction="row" gap={1.5}>
+      <BoxM variants={createMotionVar()}>
+        <Button
+          startIcon={<ArrowBackIosNewRoundedIcon fontSize="small" />}
+          variant="contained"
+          disableElevation
+          component={Link}
+          href={`/files/projects`}
+          sx={{
+            transition: "all 0.25s ease",
+            scale: "1.001",
+            "&:hover": { bgcolor: "primary.light", scale: "1.03" },
+            "&:active": { scale: "0.97" },
+          }}
+        >
+          Back to Projects
+        </Button>
+      </BoxM>
+    </Stack>
+  );
+}

a1840487691f3abecf6a5596b2858121e7bbe78b
1ureka <summerheadquarters001@gmail.com>
2024-09-06 11:37:08 +0800

    feat: Add exploreSchema for project creation and editing

---
+++ src/schema/exploreSchema.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const exploreSchema = z.object({
+  project: z.string().min(1, "Please select a group first"),
+  description: z.string().min(1, "Please enter a description"),
+  imageFields: z
+    .array(
+      z.object({
+        id: z.string().min(1, "Image ID is invalid"),
+        name: z.string().min(1, "Please select an image"),
+        camera: z
+          .number()
+          .int("Camera index must be integer")
+          .gte(0, "Camera index must be greater than or equal to 0"),
+        tag: z.string().min(1, "Please enter a tag for the image"),
+      })
+    )
+    .min(1, "Images are required"),
+});

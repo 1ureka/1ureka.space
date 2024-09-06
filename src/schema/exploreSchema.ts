ee270c9fa54f5c2d53b3ed516c60e3ab4dbc30d4
1ureka <summerheadquarters001@gmail.com>
2024-09-06 11:44:44 +0800

    refactor: Update trim() in exploreSchema.ts.

--- src/schema/exploreSchema.ts
+++ src/schema/exploreSchema.ts
@@ -1,18 +1,18 @@
 import { z } from "zod";
 
 export const exploreSchema = z.object({
-  project: z.string().min(1, "Please select a group first"),
-  description: z.string().min(1, "Please enter a description"),
+  project: z.string().trim().min(1, "Please select a group first"),
+  description: z.string().trim().min(1, "Please enter a description"),
   imageFields: z
     .array(
       z.object({
-        id: z.string().min(1, "Image ID is invalid"),
-        name: z.string().min(1, "Please select an image"),
+        id: z.string().trim().min(1, "Image ID is invalid"),
+        name: z.string().trim().min(1, "Please select an image"),
         camera: z
           .number()
           .int("Camera index must be integer")
           .gte(0, "Camera index must be greater than or equal to 0"),
-        tag: z.string().min(1, "Please enter a tag for the image"),
+        tag: z.string().trim().min(1, "Please enter a tag for the image"),
       })
     )
     .min(1, "Images are required"),

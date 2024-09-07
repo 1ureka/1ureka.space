f7893b325c7cea3c48a7109a73009a5e21d132ae
1ureka <summerheadquarters001@gmail.com>
2024-09-07 19:13:37 +0800

    refactor: Update files/projects page to get real data.

--- src/app/(content)/files/(children)/projects/page.tsx
+++ src/app/(content)/files/(children)/projects/page.tsx
@@ -1,28 +1,17 @@
 import { validateSession } from "@/auth";
+import { getAllExploreMetadata } from "@/data/metadata";
 import Block from "@/components/Block";
 import Table from "@/components/(files)/ExploreTable";
-import { delay } from "@/utils/server-utils";
-
-const getFakeMetadata = async () => {
-  await delay(Math.random() * 1000);
-
-  const randomInt = (min: number, max: number) =>
-    Math.floor(Math.random() * (max - min + 1)) + min;
-
-  return Array.from({ length: 12 }, (_, i) => ({
-    id: i.toString(),
-    group: `Project ${randomInt(1, 5)}`,
-    camera: randomInt(1, 3),
-    tag: `Tag ${randomInt(1, 5)}`,
-    detail: `Description ${randomInt(1, 5)}`,
-    metadataId: i.toString(),
-  }));
-};
 
 export default async function Page() {
   await validateSession();
 
-  const metadataList = await getFakeMetadata();
+  const metadataList = (await getAllExploreMetadata()).map(
+    ({ metadata, ...data }) => ({
+      ...data,
+      group: metadata.group,
+    })
+  );
 
   return (
     <Block sx={{ gridArea: "content" }}>
